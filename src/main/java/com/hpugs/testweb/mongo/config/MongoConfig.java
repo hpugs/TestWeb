2f85add67526e8b230918df4fdd34f4ab9d3fd5f
xinge <ai_hpugs@163.com>
2024-11-06 21:05:02 +0800

    优化mongoDB连接

--- src/main/java/com/hpugs/testweb/mongo/config/MongoConfig.java
+++ src/main/java/com/hpugs/testweb/mongo/config/MongoConfig.java
@@ -1,5 +1,6 @@
 package com.hpugs.testweb.mongo.config;
 
+import com.mongodb.ConnectionString;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
@@ -8,18 +9,33 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 @Configuration
 public class MongoConfig {
 
-    @Value("${mongo.connectionAddress}")
-    private String connectionAddress;
+    @Value("${mongo.host}")
+    private String host;
+
+    @Value("${mongo.port}")
+    private Integer port;
+
+    @Value("${mongo.username}")
+    private String username;
+
+    @Value("${mongo.password}")
+    private String password;
 
     @Value("${mongo.databaseName}")
     private String databaseName;
 
     @Bean(name = "mongoClient")
-    MongoClient init() {
-        MongoClient mongoClient = MongoClients.create(connectionAddress);
+    MongoClient init() throws UnsupportedEncodingException {
+        String usernameEncoder = URLEncoder.encode(username, "utf-8");
+        String passwordEncoder = URLEncoder.encode(password, "utf-8");
+        ConnectionString connectionString = new ConnectionString("mongodb://" + usernameEncoder + ":" + passwordEncoder + "@" + host + ":" + port);
+        MongoClient mongoClient = MongoClients.create(connectionString);
         return mongoClient;
     }
 
